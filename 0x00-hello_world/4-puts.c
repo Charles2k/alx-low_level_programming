a90637e124b4cfc9a3b0d2e8713ceef7aa0c1ffe
Charles2k <ejeabochachikodili@gmail.com>
2022-10-13 14:04:30 -0700

    C programs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - Print the string with the function puts
+  *
+  *Description- This program will print
+  *"Programming is like building a multilingual puzzle"
+  *using the puts function
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  *main - This will print the string using the printf function
+  *
+  *Description - This program should print
+  *"with proper grammar, but the outcome is a piece of art,"
+  *to the output
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
