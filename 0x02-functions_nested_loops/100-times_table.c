7817ea311f215da02d5007a5804676ff6e82ad25
Charles2k <ejeabochachikodili@gmail.com>
2022-10-19 22:55:51 +0000

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -34,11 +34,10 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0'); }
-				else if (k == 0 &&  j == 0)
+				else if (k == 0 &&  j == 0 && n != 0)
 				{
 					_putchar(k + '0');
-					_putchar(',');
-					_putchar(' '); }
+					_putchar(','); }
 				if (j < n && j != 0)
 				_putchar(',');
 				_putchar(' ');
