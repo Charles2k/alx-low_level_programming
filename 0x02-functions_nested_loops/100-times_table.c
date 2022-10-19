5e350f4551275a18bf36153de2a53de8fe245dfd
Charles2k <ejeabochachikodili@gmail.com>
2022-10-19 20:41:23 +0000

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,8 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
+	if (n >= 0 && n <= 15)
+	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
@@ -16,29 +18,34 @@ void print_times_table(int n)
 				if (k == 0)
 				{
 					_putchar(k + '0');
+					_putchar(',');
+					_putchar(' ');
 				}
 				else if (k < 10 && k > 0)
 				{
 					_putchar(k + '0');
+					_putchar(',');
+					_putchar(' ');
 				}
 				else if (k >= 10 && k < 100)
 				{
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
 				}
 				else if	(k >= 100)
 					{
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
-					}
-				else if (n < 0 && n > 15)
-				{
+					_putchar(',');
 					_putchar(' ');
+					}
 				_putchar(',');
 				_putchar(' ');
 			}
-			_putchar('\n');
 		}
+			_putchar('\n')
 	}
 }
