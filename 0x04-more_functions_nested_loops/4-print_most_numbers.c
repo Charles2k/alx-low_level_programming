a215708435a24894fa3da6c920973b95b5098327
Charles2k <ejeabochachikodili@gmail.com>
2022-10-20 20:38:39 +0000

    more loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-  *print_most_numbers - prints only allowed numbers
-  *
-  *description - prints all numbers except 2 amd 4
-  *
-  *Return: Always 0 (Success)
-  */
+ *print_most_numbers - prints only allowed numbers
+ *
+ *description - prints all numbers except 2 amd 4
+ *
+ *Return: Always 0 (Success)
+ */
 void print_most_numbers(void)
 {
 	int n;
