149bc3412d31513fc0b12e648959e68abdb973a2
Charles2k <ejeabochachikodili@gmail.com>
2022-10-20 20:36:57 +0000

    more loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,12 +10,9 @@
  */
 void print_numbers(void)
 {
-	int a;
+	int a = 0;
 
-	for (a = 0; a <= 9; a++)
-	{
-		_putchar('a');
-		_putchar('\n');
-	}
-	return (0);
+	while (a < 10)
+		_putchar(a++ + '0');
+	_putchar('\n');
 }
