a64a626ee2ccb8be87cdbf60d2c888bb1b16b9f1
Charles2k <ejeabochachikodili@gmail.com>
2022-10-20 20:23:33 +0000

    more loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
-  *print_numbers - prints numbers
-  *
-  *description - prints numbers 0-9 and
-  *a new line
-  *
-  *Return: Always 0 (Success)
-  */
+ *print_numbers - prints numbers
+ *
+ *description - prints numbers 0-9 and
+ *a new line
+ *
+ *Return: Always 0 (Success)
+ */
 void print_numbers(void)
 {
 	int a;
