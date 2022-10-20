6c498528f70c1f0d0fa3d4b030e9cf8b436920de
Charles2k <ejeabochachikodili@gmail.com>
2022-10-20 20:43:20 +0000

    more loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,13 @@
  */
 void print_most_numbers(void)
 {
-	int n;
+	int n = 0;
 
-	for (n >= 0; n <= 9; n++)
+	while (n < 10)
 	{
-		if (n != 2 || n != 4)
-		{
-			_putchar('n');
-			_putchar('\n');
-		}
+		if (n != 2 && n != 4)
+			_putchar(n + '0');
+		a++;
 	}
-	return (0);
+	_putchar('\n');
 }
