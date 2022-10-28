03a661dad5b5ab74b5f42d24bafe48bcd413c98c
Charles2k <ejeabochachikodili@gmail.com>
2022-10-28 00:56:22 +0000

    tried again

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,36 +7,15 @@
  */
 void print_number(int n)
 {
-	int m; /* power of 10 */
-	int c; /* boolean check */
-	int num; /* convert int to long */
+	unsigned int num; /* convert int to long */
 
-	num = n;
 	if (num < 0)
 	{
 		num *= -1;
 		_putchar('-');
 	}
-	m = 1;
-	c = 1;
-	while (c)
-	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
-	}
 	while (num >= 0)
 	{
-		if (m == 1)
-		{
 			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
 	}
 }
